6d51189b1dd55ebb3008c71acf2da2ba66e08f4b
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2014-06-25 11:43:33 +0400

    perfoemance models and decorators

---
+++ spec/performance/models.rb
@@ -0,0 +1,21 @@
+require "./performance/active_record"
+
+class Product < ActiveRecord::Base
+  def self.sample_class_method
+    "sample class method"
+  end
+
+  def hello_world
+    "Hello, World"
+  end
+end
+
+class FastProduct < ActiveRecord::Base
+  def self.sample_class_method
+    "sample class method"
+  end
+
+  def hello_world
+    "Hello, World"
+  end
+end
