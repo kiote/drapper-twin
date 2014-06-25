40579fd3308091bc5dcce83ab1d0e22c4470ee47
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2014-06-25 12:01:35 +0400

    integration tests added

---
+++ spec/integration/integration_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+require 'support/dummy_app'
+require 'support/mathcers/have_text'
+
+app = DummyApp.new(ENV["RAILS_ENV"])
+spec_types = {
+  view: ["/posts/1", "PostsController"],
+  mailer: ["posts/1/mail", "PostMailer"]
+}
+
+app.start_server do
+  spec_types.each do |type, (path, controller)|
+    page = app.get(path)
+
+    decribe "in a #{type}" do
+      it "runs in the correct environment" do
+        expect(page).to have_text(app.evironment).in("#environment")
+      end
+
+      it "uses the correct view context controller" do
+        expect(page).to have_text(controller).in("#controller")
+      end
+
+      it "can use build-in helpers" do
+        expect(page).to have_text("Once upon a...").in("#truncated")
+      end
+
+      it "can use build-in private helpers" do
+        expect(page).to have_text("<script>danger</script>").in("#html_escaped")
+      end
+
+      it "can use user-defined helpers from app/helpers" do
+        expect(page).to have_text("Hello, world!").in("#hello_world")
+      end
+
+      it "can use user-defined helpers from the controller" do
+        expect(page).to have_text("Goodnight, moon!").in("#goodnight_moon")
+      end
+
+      it "can be passed to path helpers" do
+        expect(page).to have_text("/en/posts/1").in("#path_with_id")
+      end
+
+      it "can be passed to url helpers" do
+        expect(page).to have_text("http://example.com:12345/en/posts/1").in("url_with_decorator")
+      end
+
+      it "can use url helpers with a model" do
+        expect(page).to have_text("http://www.example.com:12345/en/posts/1").in("#url_with_model")
+      end
+
+      it "can use url helpers with an id" do
+        expect(page).to have_text("http://www.example.com:12345/en/posts/1").in("#url_with_id")
+      end
+    end
+  end
+end
